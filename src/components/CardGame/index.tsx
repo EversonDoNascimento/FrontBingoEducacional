72867a7f2a77687a765b8a25e5a9dc89997da3e6
EversonDoNascimento <everson25nascimento@gmail.com>
2024-05-07 01:55:12 -0300

    feat: add home page

---
+++ src/components/CardGame/index.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import BgCardGame from "./../../../public/icons/bg-cardGame.png";
+type Props = {
+  data: { title: string; categoria: string; id: number };
+  sendId: (id: number) => void;
+};
+const CardGame = ({ data, sendId }: Props) => {
+  return (
+    <div
+      onClick={() => {
+        sendId(data.id);
+      }}
+      className=" cursor-pointer flex flex-col bg-[#1A1B1F] pb-2 rounded-xl hover:scale-105 transition-all ease-linear duration-200 "
+    >
+      <Image
+        className="rounded-t-xl"
+        src={BgCardGame}
+        alt="Background card"
+        width={180}
+      ></Image>
+      <div className="flex flex-col my-4 mx-2">
+        <p className="text-sm w-40 text-nowrap overflow-hidden">
+          Nome: {data.title}
+        </p>
+        <p className="text-sm w-40  text-nowrap overflow-hidden">
+          Categoria: {data.categoria}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CardGame;
