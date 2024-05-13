a092abfda9d5eaf000e1d4fbc0c3bf4c079a86fe
Brazf <fabiobrazferreira@gmail.com>
2024-05-12 21:10:03 -0300

    feat: add component InitGame

--- src/components/EditPassword/index.tsx
+++ src/components/EditPassword/index.tsx
@@ -1,18 +1,64 @@
 "use client";
-import { useForm } from "react-hook-form";
-import AccessComponent from "../AccessComponent";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import Image from "next/image";
-import EmailIcon from "./../../../public/icons/icon-email.png";
-import Link from "next/link";
+import { useState } from "react";
 import Header from "../Header/Header";
 
 const EditPassword = () => {
-  
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleModifyClick = () => {
+    if (password !== confirmPassword) {
+      setErrorMessage("As senhas não coincidem.");
+      ("As senhas não coincidem.");
+    } else {
+      // Faça algo com as senhas válidas, como enviar para o servidor
+      setErrorMessage("");
+    }
+  };
+
   return (
     <>
-    <Header></Header>
+      <Header />
+      <main className="flex flex-col items-center lg:p-24 p-0">
+        <h1 className="text-4xl">Perfil</h1>
+        <div className="pt-10 space-y-4">
+          <label className="bg-[#1A1B1F] w-[100%] outline-none">
+            Editar senha:
+          </label>
+          <input
+            placeholder="Senha"
+            className="
+            bg-transparent 
+            flex border-[1px] px-4 py-2 rounded-lg lg:w-96 w-80 placeholder-white"
+            onChange={handlePasswordChange}
+          ></input>
+          <input
+            placeholder="Confirmar a senha"
+            className="
+            bg-transparent flex border-[1px] px-4 py-2 rounded-lg lg:w-96 w-80 placeholder-white"
+            onChange={handleConfirmPasswordChange}
+          ></input>
+          {errorMessage && (
+            <p className="text-red-500">{errorMessage}</p>
+          )}
+        </div>
+
+        <button
+          className="mt-5 text-center rounded-lg px-6 py-1 bg-[#3D4EFB] hover:scale-105 transition-all ease-linear duration-200"
+          onClick={handleModifyClick}
+        >
+          MODIFICAR
+        </button>
+      </main>
     </>
   );
 };
